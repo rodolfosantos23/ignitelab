a2c8a0ce17528603d673a3b7008551721dd2a064
Rodolfo Santos <rodolfosantos23@gmail.com>
2022-06-22 16:54:33 -0300

    Criando o Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,9 @@
-import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react";
-import { client } from "./lib/apollo";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-      teacher {
-        name
-      }
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event";
 
 function App() {
-  // Retorna um objeto lessons: Array de Lesson
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
   return (
     <div>
-      <ul>
-        {data?.lessons.map((lesson) => {
-          return <li key={lesson.id}>{lesson.title}</li>;
-        })}
-      </ul>
+      <Event />
     </div>
   );
 }
