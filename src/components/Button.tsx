da338eee9b24f2b2caf170f549d84bef78e34f1d
Rodolfo Santos <rodolfosantos23@gmail.com>
2022-06-23 17:34:14 -0300

    Melhoria Button Component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,18 @@
+import { IconProps } from "phosphor-react";
+
 interface ButtonProps {
   link: string;
   variant: "success" | "info";
   text: string;
-  logo: any;
+  logo: React.ForwardRefExoticComponent<
+    IconProps & React.RefAttributes<SVGSVGElement>
+  >;
+  logoSize?: number;
 }
 
 export function Button(props: ButtonProps) {
   const Logo = props.logo;
+  const size = props.logoSize ? props.logoSize : 20;
 
   return (
     <span>
@@ -15,7 +21,7 @@ export function Button(props: ButtonProps) {
           href="#"
           className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
         >
-          <Logo size={20} />
+          <Logo size={size} />
           {props.text}
         </a>
       )}
@@ -25,7 +31,7 @@ export function Button(props: ButtonProps) {
           href="#"
           className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors"
         >
-          <Logo size={20} />
+          <Logo size={size} />
           {props.text}
         </a>
       )}
